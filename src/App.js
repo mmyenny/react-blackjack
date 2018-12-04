9eb1ffc37806764ffdd6b10e0a104fc1012c4024
Michelle Yenny <mmy491@gmail.com>
2018-12-04 09:59:59 -0500

    plan

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        <h1>Blackjack</h1>
+        <div className="center">
+          <p className="game-results">Test Your Skills!</p>
+        </div>
+        <div className="center">
+          <button className="reset hidden">Play Again!</button>
+        </div>
+
+        <div className="play-area">
+          <div className="left">
+            <button className="hit">Hit</button>
+            <p>Your Cards:</p>
+            <p className="player-total">Total 0</p>
+            <div className="player-hand" />
+          </div>
+
+          <div className="right">
+            <button className="stay">Stay</button>
+            <p>Dealer Cards:</p>
+            <p className="dealer-total">Facedown</p>
+            <div className="dealer-hand">
+              <img
+                className="cardback-one"
+                alt="card"
+                src="./images/card back red.png"
+              />
+              <img
+                className="cardback-two"
+                alt="card"
+                src="./images/card back red.png"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    )
   }
 }
 
-export default App;
+export default App
